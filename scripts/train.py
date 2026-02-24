0ae20210d0e2636b29ffe61ee9228cdc0071304c
Mattia Scardecchia <mattia.scardecchia@nyu.edu>
2026-02-23 22:38:30 -0500

    fix: configs

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,8 @@
 """Usage:
-    python scripts/train.py --config-name=config/vae
-    python scripts/train.py --config-name=config/diffusion data=spiral
-    python scripts/train.py --config-name=config/flow_matching architecture=residual_mlp
-    python scripts/train.py --config-name=config/ebm trainer=debug logger=none
+    python scripts/train.py
+    python scripts/train.py -cn diffusion
+    python scripts/train.py -cn flow_matching architecture=residual_mlp
+    python scripts/train.py -cn ebm trainer=debug logger=none
 """
 
 import hydra
@@ -10,7 +10,7 @@ from omegaconf import DictConfig, OmegaConf
 from src.utils import train
 
 
-@hydra.main(version_base="1.3", config_path="../configs", config_name="config/vae")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="diffusion")
 def main(cfg: DictConfig) -> float | None:
     print(OmegaConf.to_yaml(cfg))
     return train(cfg)
