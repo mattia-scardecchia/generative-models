64769d0016ead3bcc601949d76750206e70181d2
Mattia Scardecchia <mattia.scardecchia@nyu.edu>
2026-02-23 22:25:18 -0500

    chore: configs

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,8 @@
 """Usage:
-    python scripts/train.py
-    python scripts/train.py trainer=debug logger=none
-    python scripts/train.py model.beta=4.0 data.noise=0.1
-    python scripts/train.py --multirun hydra/launcher=submitit_slurm model.beta=0.1,0.5,1.0
+    python scripts/train.py --config-name=config/vae
+    python scripts/train.py --config-name=config/diffusion data=spiral
+    python scripts/train.py --config-name=config/flow_matching architecture=residual_mlp
+    python scripts/train.py --config-name=config/ebm trainer=debug logger=none
 """
 
 import hydra
@@ -10,7 +10,7 @@ from omegaconf import DictConfig, OmegaConf
 from src.utils import train
 
 
-@hydra.main(version_base="1.3", config_path="../configs", config_name="config")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="config/vae")
 def main(cfg: DictConfig) -> float | None:
     print(OmegaConf.to_yaml(cfg))
     return train(cfg)
